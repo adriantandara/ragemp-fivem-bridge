17f663b02c2d62c34da05d6645ce8ba5c9ced7a3
AdrianOficial <snapkwi@gmail.com>
2026-05-29 21:00:59 +0300

    Fixed some missing natives + fixed [] calls on .call

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,12 +3,14 @@ import * as fsCompat from "./Plugins/builtin/fs-compat";
 import * as envLoader from "./Plugins/builtin/env-loader";
 import * as spawnmanager from "./Plugins/builtin/spawnmanager";
 import * as vehicleSync from "./Plugins/builtin/vehicle-sync";
+import * as rageRpc from "./Plugins/builtin/rage-rpc";
 
 if (GetCurrentResourceName() !== "ragemp-fivem-bridge") {
   globalThis.mp = new Mp();
 
   globalThis.mp.plugins.registerBuiltin(fsCompat);
   globalThis.mp.plugins.registerBuiltin(envLoader);
+  globalThis.mp.plugins.registerBuiltin(rageRpc);
   globalThis.mp.plugins.registerBuiltin(spawnmanager);
   globalThis.mp.plugins.registerBuiltin(vehicleSync);
   globalThis.mp.plugins.loadAll();
